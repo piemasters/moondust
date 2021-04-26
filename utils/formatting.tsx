a7817125fcd006cd54cbd636a1b70b81c41aebf2
David Norton <pie_masters@hotmail.co.uk>
2021-04-26 18:15:34 +0100

    feat: refactor formatCoinData

--- utils/formatting.tsx
+++ utils/formatting.tsx
@@ -19,45 +19,49 @@ export const formatCoinData = (res) => {
     image: coinRawData?.image?.small ?? "-",
     link: coinRawData?.links?.homepage[0] ?? "-",
     description: coinRawData?.description?.en ?? "-",
-    price: {
-      ranges: ["1D", "7D", "30D", "200D"],
-      percentages: [
-        coinRawData?.market_data?.price_change_percentage_24h ?? "-",
-        coinRawData?.market_data?.price_change_percentage_7d ?? "-",
-        coinRawData?.market_data?.price_change_percentage_30d ?? "-",
-        coinRawData?.market_data?.price_change_percentage_200d ?? "-",
-      ],
-      asCurrency: [
-        coinRawData?.market_data?.price_change_percentage_24h_in_currency
-          ?.gbp ?? "-",
-        coinRawData?.market_data.price_change_percentage_7d_in_currency?.gbp ??
-          "-",
-        coinRawData?.market_data.price_change_percentage_30d_in_currency?.gbp ??
-          "-",
-        coinRawData?.market_data.price_change_percentage_200d_in_currency
-          ?.gbp ?? "-",
-      ],
-      low: coinRawData?.market_data?.low_24h?.gbp
-        ? formatCurrency(coinRawData.market_data.low_24h.gbp)
-        : "-",
-      high: coinRawData?.market_data?.high_24h?.gbp
-        ? formatCurrency(coinRawData.market_data.high_24h.gbp)
-        : "-",
-    },
-    popularity: {
-      upVotes: coinRawData?.sentiment_votes_up_percentage ?? "-",
-      redditSubscribers: coinRawData?.community_data.reddit_subscribers
-        ? coinRawData.community_data.reddit_subscribers.toLocaleString()
-        : "-",
-      redditComments: coinRawData?.community_data?.reddit_average_comments_48h
-        ? Math.round(
-            coinRawData.community_data.reddit_average_comments_48h
-          ).toLocaleString()
-        : "-",
-      rank: coinRawData?.coingecko_rank ?? "-",
-      stars: coinRawData?.developer_data?.stars
-        ? coinRawData.developer_data.stars.toLocaleString()
-        : "-",
-    },
+    price: getCoinPriceDetails(coinRawData),
+    popularity: getCoinPopularityDetails(coinRawData),
+  };
+};
+
+const getCoinPriceDetails = (coin) => {
+  return {
+    ranges: ["1D", "7D", "30D", "200D"],
+    percentages: [
+      coin?.market_data?.price_change_percentage_24h ?? "-",
+      coin?.market_data?.price_change_percentage_7d ?? "-",
+      coin?.market_data?.price_change_percentage_30d ?? "-",
+      coin?.market_data?.price_change_percentage_200d ?? "-",
+    ],
+    asCurrency: [
+      coin?.market_data?.price_change_percentage_24h_in_currency?.gbp ?? "-",
+      coin?.market_data.price_change_percentage_7d_in_currency?.gbp ?? "-",
+      coin?.market_data.price_change_percentage_30d_in_currency?.gbp ?? "-",
+      coin?.market_data.price_change_percentage_200d_in_currency?.gbp ?? "-",
+    ],
+    low: coin?.market_data?.low_24h?.gbp
+      ? formatCurrency(coin.market_data.low_24h.gbp)
+      : "-",
+    high: coin?.market_data?.high_24h?.gbp
+      ? formatCurrency(coin.market_data.high_24h.gbp)
+      : "-",
+  };
+};
+
+const getCoinPopularityDetails = (coin) => {
+  return {
+    upVotes: coin?.sentiment_votes_up_percentage ?? "-",
+    redditSubscribers: coin?.community_data.reddit_subscribers
+      ? coin.community_data.reddit_subscribers.toLocaleString()
+      : "-",
+    redditComments: coin?.community_data?.reddit_average_comments_48h
+      ? Math.round(
+          coin.community_data.reddit_average_comments_48h
+        ).toLocaleString()
+      : "-",
+    rank: coin?.coingecko_rank ?? "-",
+    stars: coin?.developer_data?.stars
+      ? coin.developer_data.stars.toLocaleString()
+      : "-",
   };
 };
